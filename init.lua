eb16fefda032e1bb2d4043df64ea30728f548d3c
max-desousa <max.de.sousa@outlook.com>
2025-01-26 09:32:59 -0500

    Initialization of my nvim config using lazy.nvim

---
+++ init.lua
@@ -0,0 +1,31 @@
+require("config.lazy")
+print("Welcome PaperJammer!")
+
+--[[
+vim.cmd("colorscheme zaibatsu")
+--]]
+vim.cmd("colorscheme kanagawa")
+
+vim.cmd("set cc=80")
+vim.cmd("set cursorcolumn")
+vim.cmd("set cursorline")
+
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+
+vim.cmd("set autoindent")
+vim.cmd("set expandtab")
+vim.cmd("set smarttab")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=2")
+
+vim.cmd("set listchars=tab:<->,space:·,eol:$,extends:»,precedes:«")
+vim.cmd("set list")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "netrw",
+  callback = function()
+    vim.wo.number = true
+    vim.wo.relativenumber = true
+  end
+})
