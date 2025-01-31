66cc0fb0d4d406a4b9ece67991e086c598602a9c
max-desousa <max.de.sousa@outlook.com>
2025-01-30 22:53:09 -0500

    Adding copilot and fixing treesitter

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -1,3 +1,17 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  build = ":TSUpdate",
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "go" },
+
+      sync_install = false,
+
+      auto_install = false,
+
+      highlight = {
+        enable = true,
+      },
+    }
+  end,
 }
