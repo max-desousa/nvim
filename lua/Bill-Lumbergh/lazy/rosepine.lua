9ecb190d2b2251c4db8e87d67639a5a195ba97e9
max-desousa <max.de.sousa@outlook.com>
2025-07-08 18:27:35 -0400

    Adding some transparency stuff to try.

--- lua/Bill-Lumbergh/lazy/rosepine.lua
+++ lua/Bill-Lumbergh/lazy/rosepine.lua
@@ -4,5 +4,7 @@ return {
   name = "rose-pine",
   config = function()
     vim.cmd("colorscheme rose-pine")
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- Make background transparent
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- Make background transparent
   end
 }
