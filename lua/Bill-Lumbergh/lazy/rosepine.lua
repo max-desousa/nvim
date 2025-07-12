6a796506649d3c605fbc377a52fdd2c97f22d113
max-desousa <max.de.sousa@outlook.com>
2025-07-12 09:41:01 -0400

    Adding transparency for the rest of the rosepine scheme

--- lua/Bill-Lumbergh/lazy/rosepine.lua
+++ lua/Bill-Lumbergh/lazy/rosepine.lua
@@ -4,7 +4,20 @@ return {
   name = "rose-pine",
   config = function()
     vim.cmd("colorscheme rose-pine")
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- Make background transparent
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- Make background transparent
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- Make background transparent
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- Make background transparent
+    vim.api.nvim_set_hl(0, "Normal", { bg = nil, ctermbg = nil })  -- Non-current windows
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = nil, ctermbg = nil })  -- Non-current windows
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = nil, ctermbg = nil })  -- Floating windows/panes
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = nil, ctermbg = nil })  -- Float borders
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = nil, ctermbg = nil })  -- Sign/gutter column
+    vim.api.nvim_set_hl(0, "LineNr", { bg = nil, ctermbg = nil })  -- Line numbers
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = nil, ctermbg = nil })  -- Buffer end (~ lines)
+    vim.api.nvim_set_hl(0, "StatusLine", { bg = nil, ctermbg = nil })  -- Statusline
+    vim.api.nvim_set_hl(0, "StatusLineNC", { bg = nil, ctermbg = nil })  -- Non-current statuslineackground transparent
+
+    vim.o.winblend = 20
+
+    vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = nil, ctermbg = nil })  -- Telescope normal
   end
 }
