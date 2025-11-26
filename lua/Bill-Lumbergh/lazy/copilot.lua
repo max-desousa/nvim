a604bc233a0c0bcf2a8de11ceddcbf696ef6245f
Max DeSousa <maxdesousa@protonmail.com>
2025-11-26 18:21:16 -0500

    Trying ability to turn on/off copilot
    Annoying messages about lack of autocomplete suck... trying this
    to enable/disable that.

--- lua/Bill-Lumbergh/lazy/copilot.lua
+++ lua/Bill-Lumbergh/lazy/copilot.lua
@@ -1,3 +1,14 @@
 return {
-  "github/copilot.vim"
+  "github/copilot.vim",
+
+  init = function()
+    vim.g.copilot_enabled = false
+    -- vim.g.copilot_no_tab_map = true
+  end,
+
+  keys = {
+    { "<leader>cp", "<cmd>Copilot enable<CR>", desc = "Copilot: enable" },
+    { "<leader>cd", "<cmd>Copilot disable<CR>", desc = "Copilot: disable" },
+  },
 }
+
